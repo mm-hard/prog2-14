e5eb167e8ef45b579b8dddb44b9fa3c4a759cdd1
Mahiro Mizota <j3367ofn@sit.ac.jp>
2024-12-17 16:35:40 +0900

    modelsの修正

--- models.py
+++ models.py
@@ -24,14 +24,14 @@ def test_accuracy(model,dataloader,device='cpu'):
     n_corrects=0  #正解の個数
 
     #追加
-    model.to(device)
+    model=model.to(device)
 
     model.eval()
     for image_batch,label_batch in dataloader:
 
         #追加
-        image_batch.to(device)
-        label_batch.to(device)
+        image_batch=image_batch.to(device)
+        label_batch=label_batch.to(device)
 
         with torch.no_grad():
             logits_batch=model(image_batch)
@@ -45,11 +45,13 @@ def test_accuracy(model,dataloader,device='cpu'):
 
 def train(model,dataloader,loss_fn,optimizer,device='cpu'):
     model.train()
-    model.to(device)
+
+    model=model.to(device)
+
     for image_batch,label_batch in dataloader:
 
-        image_batch.to(device)
-        label_batch.to(device)
+        image_batch=image_batch.to(device)
+        label_batch=label_batch.to(device)
 
         logits_batch=model(image_batch)
 
@@ -66,13 +68,15 @@ def train(model,dataloader,loss_fn,optimizer,device='cpu'):
 
 def test(model,dataloader,loss_fn,device='cpu'):
     loss_total=0.0
-    model.to(device)
+
+    model=model.to(device)
+    
     model.eval()
     for image_batch,label_batch in dataloader:
 
-        image_batch.to(device)
-        label_batch.to(device)
-        
+        image_batch=image_batch.to(device)
+        label_batch=label_batch.to(device)
+
         with torch.no_grad():
             logits_batch=model(image_batch)
 
